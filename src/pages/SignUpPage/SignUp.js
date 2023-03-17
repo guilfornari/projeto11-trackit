aedb4a0bbb9f37afa4b5574f09d7f9b62859c3a1
Guilfornari <guilfornari@gmail.com>
2023-03-16 23:02:10 -0300

    Started implementing functions on signup page

--- src/pages/SignUpPage/SignUp.js
+++ src/pages/SignUpPage/SignUp.js
@@ -1,9 +1,25 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../components/Logo/Logo";
 import { PageContainer } from "./signUpStyle";
 
 
 export default function SignUp() {
+
+    const [userInfo, setUserInfo] = useState(
+        {
+            email: "",
+            name: "",
+            image: "",
+            password: ""
+        }
+    )
+
+
+    function createUser() {
+
+    }
+
     return (
         <PageContainer>
 
@@ -11,21 +27,29 @@ export default function SignUp() {
 
             <form onSubmit={() => alert("Sent!")}>
                 <input
+                    value={userInfo.email}
+                    onChange={e => setUserInfo(e.target.value)}
                     type="email"
                     placeholder="email"
                     required
                     data-test="email-input" />
                 <input
+                    value={userInfo.password}
+                    onChange={e => setUserInfo(e.target.value)}
                     type="password"
                     placeholder="senha"
                     required
                     data-test="password-input" />
                 <input
+                    value={userInfo.name}
+                    onChange={e => setUserInfo(e.target.value)}
                     type="text"
                     placeholder="nome"
                     required
                     data-test="user-name-input" />
                 <input
+                    value={userInfo.image}
+                    onChange={e => setUserInfo(e.target.value)}
                     type="url"
                     placeholder="foto"
                     required
